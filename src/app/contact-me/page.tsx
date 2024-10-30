ebd944c09a6b0b360a1e010dab4aab92f04f1ec5
Jared588 <jaredeprice@gmail.com>
2024-10-30 02:05:16 +0200

    Add contact page

---
+++ src/app/contact-me/page.tsx
@@ -0,0 +1,15 @@
+import Header from "../_components/Header";
+
+export default function ContactMe() {
+  return (
+    <main className="flex min-h-screen flex-col items-center bg-gradient-to-b from-[#dddddd] to-[#b9b9b9] text-black">
+      <Header />
+      <div className="flex flex-grow flex-col justify-center gap-y-4 p-6 pb-52 text-center">
+        <h1 className="pb-8 text-5xl font-extrabold tracking-tight sm:text-[5rem]">
+          Contact <span className="text-blue-900">Me</span>
+        </h1>
+        <p className="text-3xl">📧 jaredeprice@proton.me</p>
+      </div>
+    </main>
+  );
+}
